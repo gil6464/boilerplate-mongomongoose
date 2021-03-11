67bc7a05f50c3bd2e510e88a0cf7a7b36651967b
gil6464 <gil6464@gmail.com>
2021-03-11 13:59:14 +0200

    first qwestions solve

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
-
-
+const mongoose = require('mongoose')
+const url = process.env.URL;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,
+  useFindAndModify: false, useCreateIndex: true }).then(result => {
+     console.log('connected to MongoDB')
+   })
+   .catch((error) => {
+     console.log('error connecting to MongoDB:', error.message)
+})
 let Person;
 
 const createAndSavePerson = (done) => {
